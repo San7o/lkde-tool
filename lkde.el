4b31431ae33e1acecb22c84aea7113f09bf180ad
San7o <santigio2003@gmail.com>
2025-08-10 11:27:19 +0200

    lkde: emacs integration

---
+++ lkde.el
@@ -0,0 +1,24 @@
+(setq lkde-env "linux")
+(setq lkde-base-dir "~/lkde")
+
+(defun lkde-set-env ()
+  ;; Set the lkde-env variable
+  (interactive)
+  (let ((env (read-from-minibuffer "Set environment to: ")))
+    (setq lkde-env env)
+    (message (concat "Set lkde-env variable to " env))))
+
+(defun lkde-set-base-dir ()
+  ;; Set the lkde-env variable
+  (interactive)
+  (let ((base-dir (read-directory-name "Set lkde base directory to: ")))
+    (setq lkde-base-dir base-dir)
+    (message (concat "Set lkde-base-dir variable to " base-dir))))
+
+(defun lkde ()
+  (interactive)
+  (let* ((default (concat "ENV=" lkde-env " make -C " lkde-base-dir " "))
+         (command (read-from-minibuffer "Shell command: " default)))
+    (async-shell-command command)))
+
+(provide 'lkde)
